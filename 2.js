e7576da5e10c1240cb36f7aea6d344514d14d3d2
Khusni Jafar <khusnijafar@gmail.com>
2020-12-26 18:24:10 +0700

    add 2

---
+++ 2.js
@@ -0,0 +1,13 @@
+const compareTriplets = (a,b) => {
+    let points = [0,0];
+    for (let i = 0; i < a.length; i++) {
+        if(a[i] > b[i]) {
+            points[0]++
+        } else if (a[i] !== b[i]) {
+            points[1]++
+        }
+    }
+    return points;
+}
+
+console.log(compareTriplets([5,6,7],[3,6,10]));
